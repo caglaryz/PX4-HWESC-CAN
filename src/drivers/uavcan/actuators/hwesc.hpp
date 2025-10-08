e6f9cb37083079372549d37bd38c55be82ad9af9
caglar <45522735+caglaryz@users.noreply.github.com>
2025-10-08 09:02:40 +0300

    Enhance UAVCAN ESC controller with new features
    Added subscriptions for actuator armed state and duplicate node ID check.

--- src/drivers/uavcan/actuators/hwesc.hpp
+++ src/drivers/uavcan/actuators/hwesc.hpp
@@ -58,6 +58,9 @@
 #include <uORB/PublicationMulti.hpp>
 #include <uORB/topics/actuator_outputs.h>
 
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/actuator_armed.h>
+
 #include <uORB/topics/esc_status.h>
 
 #include <drivers/drv_hrt.h>
@@ -95,6 +98,8 @@ public:
 	static int max_output_value() { return INT14_MAX; }
 
 private:
+	static constexpr uint8_t kInvalidIdx = 0xFF;   // invalid esc index
+	static constexpr uint8_t kInvalidNID = 0x00;   // invalid node id
 	struct ESCStatus {
 		bool     is_expected{false};	// expected ESCs according to _rotor_count regardless of physical actuator link
 		bool     is_hobbywing{false};	// might need it later, will assign it regardless
@@ -123,12 +128,16 @@ private:
 
 		bool 	 msg1_received{false};
 		bool 	 msg2_received{false};
-		// bool 	 msg3_received{false};
+		// bool  msg3_received{false};
 		bool	 active{false};
 	};
 
 	bool _reinit_requested{true}; 		// < trigger for mapping the escs.
 	bool _mapping_in_progress{false};
+
+	bool _armed{false};			// < whether the system is armed or not
+
+	bool _can_control_ready{false};		// < whether the system is ready for CAN control (set to true if PWM regardless for now)
 
 	// Enum for error types
  	enum EscErrorType {
@@ -140,38 +149,31 @@ private:
 		ESC_ERROR_MISMATCHED_ESC_CONFIG, // ESC configured as can_throttle but no response or vice versa
 		ESC_ERROR_UNEXPECTED_ESC_COUNT,  // More ESCs responded than configured
 		ESC_ERROR_INVALID_THROTTLE_CH,  // Invalid throttle channel in UAVCAN_EC_FUNCx parameter
-		ESC_ERROR_DUPLICATE_MAPPING,
+		ESC_ERROR_DUPLICATE_MAPPING,	// Duplicate node id mapped to same esc index
 		// TODO: add errors related to expected escs not initialized/mapped
 	};
-
-	static constexpr uint8_t kInvalidIdx = 0xFF;   // invalid esc index
-	static constexpr uint8_t kInvalidNID = 0x00;   // invalid node id
 
 	// uint8_t _esc_index_by_node_id[128]; 					// node_id (1..0x7D) -> esc_index (0..7) map
 	// uint8_t _node_id_by_index[esc_status_s::CONNECTED_ESC_MAX];		// esc_index -> node_id map
 
 	uint16_t _status_counter{0};						// < internal counter for esc status topic
 
-	static constexpr uint64_t BOOT_GRACE_US            = 2'000'000;  	// after boot/mapping, don't mark inactive too early
-	static constexpr uint64_t TIMEOUT_MSG1_US          = 2'000'000;  	// heartbeat (MSG1) timeout
-	static constexpr uint64_t TIMEOUT_MSG2_US          = 3'000'000;  	// slower, informational
-	static constexpr uint64_t CHECK_TIMEOUTS_MIN_US    = 100'000;    	// don't scan too often (10 Hz)
+	static constexpr uint64_t BOOT_GRACE_US            = 2'000'000;  		// after boot/mapping, don't mark inactive too early
+	static constexpr uint64_t TIMEOUT_MSG1_US          = 2'000'000;  		// heartbeat (MSG1) timeout
+	static constexpr uint64_t TIMEOUT_MSG2_US          = 3'000'000;  		// slower, informational
+	static constexpr uint64_t CHECK_TIMEOUTS_MIN_US    = 100'000;    		// don't scan too often (10 Hz)
 	static constexpr uint64_t GET_ESCID_COOLDOWN_US    = 1'000'000 / MAX_GET_RATE_HZ;
-	static constexpr uint64_t MAPPING_COLLECTION_WINDOW_US = 200'000; // 200 ms window to gather GetEscID responses
-	static constexpr uint64_t WARN_THROTTLE_INTERVAL_US    = 1'000'000; // 1 second
+	static constexpr uint64_t MAPPING_COLLECTION_WINDOW_US = 200'000; 		// 200 ms window to gather GetEscID responses
+	static constexpr uint64_t WARN_THROTTLE_INTERVAL_US    = 1'000'000; 		// 1 second
 
 
-	hrt_abstime _last_esc_id_query{0};					// < last time we queried for GetEscID
-	hrt_abstime _boot_time_us{hrt_absolute_time()};				// < system time since boot
-	hrt_abstime _last_timeout_check{0};					// < last time we checked for timeouts
+	hrt_abstime _last_esc_id_query{0};						// < last time we queried for GetEscID
+	hrt_abstime _boot_time_us{hrt_absolute_time()};					// < system time since boot
+	hrt_abstime _last_timeout_check{0};						// < last time we checked for timeouts
 	hrt_abstime _mapping_deadline_us{0};
 	hrt_abstime _last_invalid_index_warn{0};
 
 	ESCStatus _esc_data[MAX_ACTUATORS] {};
-
-	// Mapping node ID -> esc index
-	// bool _node_active[128] {};              				// true if node_id has responded recently
-	// bool _was_online[MAX_ACTUATORS] {};
 
 	// message callbacks
 
@@ -187,6 +189,8 @@ private:
 	int error_handler(uint8_t error_type, uint8_t val, int res);
 
 	bool is_can_throttle(uint8_t esc_index) const;
+
+	bool has_duplicate_node_id(uint8_t nid, uint8_t exclude_idx) const;
 
 	void periodic_update(const uavcan::TimerEvent &);
 
@@ -215,9 +219,12 @@ private:
 		return (idx < esc_status_s::CONNECTED_ESC_MAX) ? uint8_t(idx + 1) : 0;
 	}
 
-	uint8_t fetch_node_esc_index(uint8_t node_id);
+	uint8_t fetch_node_esc_index(uint8_t node_id) const;
+
+	bool banned_node_ids[128];	// TODO: Parse them from param once in init and param update and treat any hit as hard fail (don’t flip _can_control_ready true).
 	bool is_banned_node(uint8_t node_id) const;
 
+	// UAVCANv0 method binder aliases
 	typedef uavcan::MethodBinder < UavcanHwescController *,
                	void (UavcanHwescController::*)(const uavcan::ReceivedDataStructure<com::hobbywing::esc::StatusMsg1> &) >
                	StatusMsg1CbBinder;
@@ -246,11 +253,9 @@ private:
 
 	esc_status_s	_esc_status{};
 
-	// esc_hw_status_s	_hwesc_status{};
-
 	uORB::Publication<esc_status_s> _esc_status_pub{ORB_ID(esc_status)};
-	// uORB::Publication<esc_hw_status_s> _hwesc_status_pub{ORB_ID(esc_hw_status)};
-	// uORB::Publication<esc_hw_info> _hwesc_info_pub{ORB_ID(esc_hw_info)};
+
+	uORB::Subscription							_actuator_armed_sub{ORB_ID(actuator_armed)};
 
 	/*
 	 * libuavcan related things
